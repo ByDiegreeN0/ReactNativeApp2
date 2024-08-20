410261444dc28c169b1140d1acc0f904290f7781
ByDiegreeN0 <diegoalejandrogarcia140902@gmail.com>
2024-08-20 13:42:47 -0500

    se crearon archivos principales

---
+++ src/components/StackNavigation.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class StackNavigation extends Component {
+  render() {
+    return (
+      <View>
+        <Text>StackNavigation</Text>
+      </View>
+    )
+  }
+}
+
+export default StackNavigation
