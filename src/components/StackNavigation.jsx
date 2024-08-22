3acdc94b2781284c95eff2dc34c4095cfaeaa76a
ByDiegreeN0 <diegoalejandrogarcia140902@gmail.com>
2024-08-22 08:47:45 -0500

    se implemento parcialmente la screen de videos

--- src/components/StackNavigation.jsx
+++ src/components/StackNavigation.jsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
 import RegisScreen from  '../screens/RegisterScreen';
 import DashboardScreen from '../screens/DashboardScreen';
+import VideoScreen from '../screens/VideoScreen'
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ function MyStack() {
       <Stack.Screen name="Home" component={RegisScreen} />
       <Stack.Screen name="Notifications" component={LoginScreen} />
       <Stack.Screen name="Profile" component={DashboardScreen} />
+      <Stack.Screen name="Videos" component={VideoScreen} />
     </Stack.Navigator>
   );
 }
