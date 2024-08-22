92cf11b8a559133752cecfcff866b71cd5ea146e
Jeisson C <jeissonluffy2006@gmail.com>
2024-08-22 16:25:00 +0200

    Diego es re gay x2

--- App.jsx
+++ App.jsx
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import MyStack from './src/components/StackNavigation';
-import Dashboard from './src/screens/DashboardScreen';
 
 export default function App() {
   return (
-    <Dashboard></Dashboard>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
 
